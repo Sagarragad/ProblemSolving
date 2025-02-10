2c34787074f7b02e04e2b6694163fb66acb625f1
Sagar Ragad <sagarragad62@gmail.com>
2025-02-10 18:35:04 +0530

    Add files via upload

---
+++ Program441.java
@@ -0,0 +1,15 @@
+import java .util.*;
+
+
+class Program441
+{
+    public static void main(String Arg[])
+    {
+        int Arr[][] = new int[4][3];
+
+        System.out.println(Arr.length);
+        System.out.println(Arr[0].length);
+        
+
+    }
+}
---
+++ Program442.java
@@ -0,0 +1,20 @@
+import java .util.*;
+
+class Matrix
+{
+    public int Arr[][];
+
+    public Matrix(int iRow,int iCol)
+    {
+        Arr = new int[iRow][iCol]; 
+    }
+
+}
+class Program441
+{
+    public static void main(String Arg[])
+    {
+        
+
+    }
+}
